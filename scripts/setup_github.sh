98e21d58ec17737569226c9ed4340cfbaabea28c
tanvirr007 <md.tanvirhasannijum@gmail.com>
2024-09-29 12:33:13 +0600

    sh: set up GitHub configuration script
    Signed-off-by: tanvirr007 <md.tanvirhasannijum@gmail.com>

---
+++ scripts/setup_github.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Set up GitHub configuration
+git config --global user.email "md.tanvirhasannijum@gmail.com"
+git config --global user.name "tanvirr007"
+git config --global core.editor "nano"
+git config --global help.autocorrect 1
+
+echo "GitHub configuration set successfully."
