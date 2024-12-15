9879adccf012c7908249f6a361bd503b16885787
tanvirr007 <md.tanvirhasannijum@gmail.com>
2024-12-15 12:07:17 +0600

    sh: Termux: add pixeldrain and gofile on automation

--- scripts/termux_setup.sh
+++ scripts/termux_setup.sh
@@ -3,29 +3,30 @@
 # My required packages
 termux-change-repo
 pkg update && pkg upgrade -y
-pkg install git
-pkg install gh
-pkg install tmate
-pkg install android-tools
-pkg install termux-tools
-pkg install termux-exec
-pkg install openssh
-apt install openssl-tool
-pkg install -y root-repo
-pkg install android-tools
-pkg install -y git tsu python wpa-supplicant pixiewps iw openssl
-pkg install zsh
-pkg install curl
-pkg install rxfetch
-pkg install tmux
-pkg install htop
-pkg install byobu
-pkg install jq
+pkg install git -y
+pkg install gh -y
+pkg install tmate -y
+pkg install android-tools -y
+pkg install termux-tools -y
+pkg install termux-exec -y
+pkg install openssh -y
+apt install openssl-tool -y
+pkg install root-repo -y
+pkg install tsu python wpa-supplicant pixiewps iw openssl -y
+pkg install zsh -y
+pkg install curl -y
+pkg install rxfetch -y
+pkg install tmux -y
+pkg install htop -y
+pkg install byobu -y
+pkg install jq -y
+pkg install wget -y
+pkg install bc -y
 
-My favorite wifi tool
+# My favorite wifi tool
 git clone https://github.com/tanvir-projects-archive/OneShot.git
 
-My GutHub config
+# My GutHub config
 curl -O https://raw.githubusercontent.com/tanvirr007/scripts/main/scripts/setup_github.sh
 bash setup_github.sh
 
@@ -34,7 +35,26 @@ sh -c "$(curl -fsSL https://github.com/tanvir-projects-archive/termux-ohmyzsh/ra
 ~/.termux/colors.sh
 ~/.termux/fonts.sh
 
+# Pixeldrain
+wget https://raw.githubusercontent.com/tanvirr007/scripts/main/scripts/pixeldrain.sh -O "/data/data/com.termux/files/usr/bin/pixeldrain" && chmod +x "/data/data/com.termux/files/usr/bin/pixeldrain"
+
+# Gofile
+wget https://raw.githubusercontent.com/tanvirr007/scripts/main/scripts/gofile.sh -O "/data/data/com.termux/files/usr/bin/gofile" && chmod +x "/data/data/com.termux/files/usr/bin/gofile"
+
+# Clear terminal
 clear
+
+# Testing Pixeldrain & Gofile
+touch Pixeldrain.json
+touch Gofile.json
+
+pixeldrain Pixeldrain.json
+gofile Gofile.json
+
+echo -n " pixeldrain and gofile both are wokring -"
+
+rm Pixeldrain.json
+rm Gofile.json
 
 # Countdowm
 echo -n "Showing device specifications in "
