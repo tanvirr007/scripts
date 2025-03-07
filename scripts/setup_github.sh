bcb2fdaa1086b8becdeba40d95be38edabcfc977
tanvirr007 <43490582+tanvirr007@users.noreply.github.com>
2025-03-07 14:39:33 +0600

    sh: GitHub: update git config

--- scripts/setup_github.sh
+++ scripts/setup_github.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set up GitHub configuration
-git config --global user.email "md.tanvirhasannijum@gmail.com"
+git config --global user.email "43490582+tanvirr007@users.noreply.github.com"
 git config --global user.name "tanvirr007"
 git config --global core.editor "nano"
 git config --global help.autocorrect 1
